5744d5a34ee699325de590079702b64e0c7ac5c1
swit <sebastian.thomasd@mail.utoronto.ca>
2018-07-19 12:01:21 -0400

    Finish the Javascript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,30 @@
-function activateGallery () {
-  alert("Hello from the gallery file!");
+// Activates the image gallery
+// The main task to attach an event listener to each image in the gallery
+// and respond appropriately on click
+
+function activateGallery() {
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      // Transfer the current image property
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      let galleryInfo = document.querySelector('#gallery-info');
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
